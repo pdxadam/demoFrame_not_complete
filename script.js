16eb63c0d16f3b3fd0e7faa72202a9969ca284d0
RHSMcLain <amclain@riverdale.k12.or.us>
2025-03-05 10:12:48 -0800

    goal/task notes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var list1 = ["Inigo", "Buttercup", "Wesley"];
 var list2 = ["Han Solo", "Luke", "Leia", "Vader"];
 var ul1, ul2;
+//Goal:  To drag a character from one list to the other on the screen AND change which array it is in here in memory
+//tasks: 
+// add draggable attribute to all the draggable elements
+// create the ondrag function that assigns the proper data to the dataTransfer event
+// add an ondrag listener to those elements
+// add an ondragover function/listener to the drop zones that prevents default behavior
+// add an ondrop function/listener to drop zones that correctly shifts the element between lists
 
 window.onload = function(){
     ul1 = document.querySelector("#list1");
